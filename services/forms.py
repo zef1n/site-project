636b76a80cd6d47ed9ced5455097132037eeb80f
zef1n <icw20@mail.ru>
2024-09-13 00:36:45 +0500

    feat style in cart fix text in index.html

--- services/forms.py
+++ services/forms.py
@@ -2,15 +2,20 @@ from django import forms
 from .models import Order
 from .models import Profile
 
+from django import forms
+from .models import Order
+
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['customer_name', 'customer_email', 'telegram_username', 'additional_info']
         widgets = {
-            'customer_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ваше имя'}),
-            'customer_email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ваш email'}),
-            'service': forms.Select(attrs={'class': 'form-control'}),
+            'customer_name': forms.TextInput(attrs={'class': 'input input-bordered w-full', 'placeholder': 'Ваше ФИО'}),
+            'customer_email': forms.EmailInput(
+                attrs={'class': 'input input-bordered w-full', 'placeholder': 'Ваш email'}),
+            'telegram_username': forms.TextInput(
+                attrs={'class': 'input input-bordered w-full', 'placeholder': 'Ваш ник в telegram (@nickname)'}),
             'additional_info': forms.Textarea(
-                attrs={'class': 'form-control', 'placeholder': 'Дополнительная информация'}),
+                attrs={'class': 'textarea textarea-bordered w-full', 'placeholder': 'Дополнительная информация'}),
         }
